1f419d2666fc2365219fa310e34586a1e7c3ec1f
Md. Abdul Kader <abdulemail789@gmail.com>
2023-10-28 11:54:38 +0600

    Program-7:added external styles with vector icon

--- App.js
+++ App.js
@@ -2,10 +2,39 @@ import React, { useState } from 'react'
 import {Button, Text, View } from 'react-native';
 import externalStyle from './styles/externalStyle';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 const App=()=>{
+  const [clickedIcon, setClickedIcon] = useState("")
     return(
-      <View>
-        <Text style={[externalStyle.textStyle, externalStyle.textBg]}>Adding External Style</Text>
+      <View style={{paddingTop:50}}>
+        <Text style={[externalStyle.textStyle, externalStyle.textBg]}>External Style with icon</Text>
+
+        <Icon.Button
+              name="facebook"
+              size={28}
+              style={{justifyContent:"center"}}
+              onPress={()=>setClickedIcon("Cliked on facebook icon")}
+          />
+          <Icon.Button
+              name="github"
+              size={28}
+              style={{justifyContent:"center"}}
+              backgroundColor="black"
+              onPress={()=>setClickedIcon("Cliked on githun icon")}
+
+          />
+          <Icon.Button
+              name="linkedin"
+              size={28}
+              style={{justifyContent:"center"}}
+              backgroundColor="blue"
+              onPress={()=>setClickedIcon("Cliked on linkedin icon")}
+
+          />
+          <Text style={{backgroundColor:"#FDEC85", padding:10,marginTop:50, fontSize:20}}>{clickedIcon}</Text>
+         
+          
+          
       </View>
     )
   
