975cb0a6c90b753db896cc31e0e79f79fb644773
Md. Abdul Kader <abdulemail789@gmail.com>
2023-10-28 10:04:57 +0600

    Program-7: skiped font awsome

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Button, Text, View } from 'react-native';
 import externalStyle from './styles/externalStyle';
+
 const App=()=>{
     return(
       <View>
