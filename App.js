6e5e9712edebe40ee1f57428918e403269cf8f04
Md. Abdul Kader <abdulemail789@gmail.com>
2023-10-28 07:48:27 +0600

    Program-5: used props to pass data in function and class component of Profile

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import React, { useState } from 'react'
 import {Button, Text, View } from 'react-native';
+import Profile from './components/Profile';
 
 const App=()=>{
     const [count, setCount] = useState(0)
     return(
       <View 
       style={{flex:1, alignItems:'center', justifyContent:'center',backgroundColor:'#FAF2BA'}}>
-        <Text style={{fontSize:30}}>{count}</Text>
-        <View style={{flexDirection: 'row',}}>
-            <Button style={{fontSize:30,}} 
-                onPress={()=>setCount(count+1)}
-                title="Increase"
-                >
-            </Button>
-            <Button style={{fontSize:30}} 
-                onPress={()=>setCount(count-1)}
-                title="Decrease"
-                >
-            </Button>
-        </View>
+        <Profile data="Abdul Kader" email="abdul@gmail.com"></Profile>
       </View>
     )
   
--- components/Profile.js
+++ components/Profile.js
@@ -1,12 +1,27 @@
+import React from 'react'
 import {Text, View } from 'react-native';
 
 
-const Profile = () => {
-    return (
-        <View>
-            <Text style={{fontSize:40}}>This is Profile Component</Text>
-        </View>
-    );
-};
+// const Profile = (props) => {
+//     return (
+//         <View>
+//             <Text style={{fontSize:40}}>Profile of {props.data}</Text>
+//         </View>
+//     );
+// };
+
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text style={{fontSize:40}}>Profile of {this.props.data}</Text>
+                <Text style={{fontSize:40}}>Profile of {this.props.email}</Text>
+
+            </View>
+        );
+    }
+}
+
 
 export default Profile;
