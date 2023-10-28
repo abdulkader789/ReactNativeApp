16dd303ecbd4f61b5c4bc07fb2e0c594bd2ecfb9
Md. Abdul Kader <abdulemail789@gmail.com>
2023-10-29 03:27:46 +0600

    Program-11: showed data from state using FlatList

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { Button, FlatList, Modal, Text, View } from 'react-native';
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    }
-
-  }
-  componentDidMount() {
-    this.apiCall();
-  }
-  async apiCall() {
-    let res = await fetch('https://facebook.github.io/react-native/movies.json')
-    let data = await res.json()
-    // console.warn(data)
-    this.setState({ data: data.movies })
-
-  }
-  render() {
-    return (
-      <View style={{ flex: 1, paddingTop: 50 }}>
-        <Text style={{ fontSize: 30 }}>Api Call</Text>
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }) =>
-            <Text style={{ fontSize: 30, backgroundColor: 'yellow', margin: 20 }}>{item.title}, {item.releaseYear}</Text>
-          }
-        >
-
-        </FlatList>
-      </View >
-    )
-  }
+const App = () => {
+  const userData = [
+    { name: 'anil', email: 'anil@gmail.com' },
+    { name: 'rafi', email: 'rafi@gmail.com' },
+    { name: 'hasan', email: 'hasan@gmail.com' },
+    { name: 'john', email: 'john@gmail.com' },
+    { name: 'alice', email: 'alice@gmail.com' },
+    { name: 'emma', email: 'emma@gmail.com' },
+    { name: 'david', email: 'david@gmail.com' },
+    { name: 'linda', email: 'linda@gmail.com' },
+    { name: 'michael', email: 'michael@gmail.com' },
+    { name: 'olivia', email: 'olivia@gmail.com' },
+    { name: 'sara', email: 'sara@gmail.com' },
+    { name: 'peter', email: 'peter@gmail.com' },
+    { name: 'jennifer', email: 'jennifer@gmail.com' },
+    { name: 'kevin', email: 'kevin@gmail.com' },
+    { name: 'natalie', email: 'natalie@gmail.com' }
+  ];
+  const [user, setUser] = useState(userData)
 
 
+  return (
+    <View style={{ flex: 1, paddingTop: 50 }}>
+      <Text style={{ fontSize: 30 }}>Api Call</Text>
+      <FlatList
+        data={user}
+        renderItem={({ item }) =>
+          <Text style={{ fontSize: 30, backgroundColor: 'gray', color: 'white', margin: 20 }}>{item.name}, {item.email}</Text>
+        }
+      >
+
+      </FlatList>
+    </View >
+  )
 
 }
 
